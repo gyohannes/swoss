13dd5c7fe736291a2f6814e1fec62a35136c48a2
user <user@GZ.local>
2022-11-09 09:45:16 +0300

    updating script

--- swoss.sh
+++ swoss.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+# git clone git@repo.moh.gov.et:gebreyohannes/swoss.git
 sudo apt-get install git-core curl
 # Adding Node.js repository
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -

4ad147615441a19b08e94d73d6f0f8ce2b78d3b5
user <user@GZ.local>
2022-11-12 17:25:30 +0300

    update

--- swoss.sh
+++ swoss.sh
@@ -10,8 +10,7 @@ sudo add-apt-repository ppa:chris-lea/redis-server
 # Refresh our packages list with the new repositories
 sudo apt-get update
 # Install our dependencies for compiiling Ruby along with Node.js and Yarn
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarngit clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -19,19 +18,17 @@ echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
 rbenv install 2.7.2
 rbenv global 2.7.2
-ruby -v
-# ruby 2.7.2
 cd swoss
+sudo apt install libpq-dev
 gem install bundler
 bundle install
-sudo apt install libpq-dev
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger focal main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
 sudo apt-get install -y nginx-extras libnginx-mod-http-passenger
 if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
 sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
-sudo apt-get install postgresql postgresql-contrib libpq-dev
+sudo apt-get install postgresql postgresql-contrib
 current_user=$(whoami)	
 echo "Congratulations! Please continue setting up server manually as described below
       sudo nano /etc/nginx/conf.d/mod-http-passenger.conf
@@ -43,8 +40,7 @@ echo "Congratulations! Please continue setting up server manually as described b
       createuser --superuser --createdb --createrole --replication --pwprompt swoss
       --- set password to be: postgres
       createdb -O swoss swmoss_prod
-      -- add the following line in ~/.profile
-      export DATABASE_URL="postgres://swoss:postgres@127.0.0.1/swmoss_prod"
+      RAILS_ENV=production bundle exec rake db:create
       RAILS_ENV=production bundle exec rake db:migrate
       RAILS_ENV=production bundle exec rake db:seed
     "
